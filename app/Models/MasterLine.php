0ec06e209596267f83979af60242aed6181401fd
happymuchsin <happymuchsin@gmail.com>
2024-08-19 14:40:53 +0700

    hotfix

--- app/Models/MasterLine.php
+++ app/Models/MasterLine.php
@@ -14,6 +14,6 @@ class MasterLine extends Model
 
     public function area()
     {
-        return $this->belongsTo(MasterArea::class, 'master_area_id', 'id');
+        return $this->belongsTo(MasterArea::class, 'master_area_id', 'id')->whereNull('deleted_at');
     }
 }
